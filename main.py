49946e63b9891876830a96921f010676866e17c0
Tomoya Konishi <gea01051@yahoo.co.jp>
2025-10-15 23:59:12 +0900

    Not use st.latex

--- main.py
+++ main.py
@@ -11,7 +11,7 @@
 #  notice, disclaimer and this condition are retained.
 
 import streamlit as st
-from sympy import *
+import sympy as sp
 from spb import plot
 
 # https://home.hirosaki-u.ac.jp/relativity/理工系の数学b/sympy-で理工系の数学b/sympy-でテイラー展開・オイラーの公式/
@@ -28,39 +28,29 @@ def main():
                      "Copyright (c) 2025 by Tomoya Konishi. All rights reserved."
         }
     )
-    # https://stackoverflow.com/questions/74456419/streamlit-latex
-    st.markdown('''
-    <style>
-    .katex-html {
-        text-align: left;
-    }
-    </style>''',
-    unsafe_allow_html=True
-    )
 
     st.title("テイラー展開の可視化")
 
     fx = st.sidebar.text_input("関数 $f(x) = $", "x * cos(x)")
-    #w = st.sidebar.number_input("描画範囲 x ∈ [-w, w] の w = ", min_value=0.1, max_value=10.0, value=2.0, step=0.1)
     n = st.sidebar.number_input("テイラー展開の次数 $n = $", min_value=1, max_value=20, value=5, step=1)
     a = st.sidebar.number_input("展開中心 $a$ = ", value=0.0, step=0.1)
     if a != 0:
-        st.sidebar.warning('展開中心が$a\\ne0$の場合、展開が正しくおこなわれない場合があります。', icon="⚠️")
+        st.sidebar.warning("展開中心が$a\\ne0$の場合、展開が正しくおこなわれない場合があります。", icon="⚠️")
 
-    init_printing(order='rev-lex')
-    x = Symbol("x")
-    expr = sympify(fx)
+    sp.init_printing(order="rev-lex")
+    x = sp.Symbol("x")
+    expr = sp.sympify(fx)
     taylor = expr.series(x, a, n+1)
 
-    st.latex('もとの関数:　 f(x)=' + latex(expr))
+    st.write("もとの関数:　$f(x)=" + sp.latex(expr) + "$")
     if a != 0:
         rounded_taylor = round_expr(taylor, 3)
-        st.latex('テイラー展開: \\bar{f}(x)=' + latex(rounded_taylor))
-        st.warning('展開中心が$a\\ne0$の場合、数値を四捨五入しています。')
+        st.write("テイラー展開: $\\displaystyle\\bar{f}(x)=" + sp.latex(rounded_taylor) + "$")
+        st.warning("展開中心が $a\\ne0$ の場合、数値を四捨五入しています。")
     else:
-        st.latex('テイラー展開: \\bar{f}(x)=' + latex(taylor))
+        st.write("テイラー展開: $\\displaystyle\\bar{f}(x)=" + sp.latex(taylor) + "$")
 
-    p = plot((expr, "$"+latex(expr)+"$"), (taylor.removeO(), "Taylor"), (x, -10, 10), ylim=(-10, 10), show=False)
+    p = plot((expr, "$" + latex(expr) + "$"), (taylor.removeO(), "Taylor"), (x, -10, 10), ylim=(-10, 10), show=False)
     st.pyplot(p.fig)
 
 
@@ -68,7 +58,7 @@ def round_expr(expr, num_digits):
     """
     Rounds all numerical terms in a SymPy expression to a specified number of digits.
     """
-    return expr.xreplace({n: round(n, num_digits) for n in expr.atoms(Number)})
+    return expr.xreplace({n: round(n, num_digits) for n in expr.atoms(sp.Number)})
 
 if __name__ == "__main__":
     main()
